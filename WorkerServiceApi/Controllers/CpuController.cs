f7ec145f3be8c4be2234f876237b02fe838ee185
derian95 <dfhamezquita@gmail.com>
2023-04-09 20:46:51 -0500

    init

---
+++ WorkerServiceApi/Controllers/CpuController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Management;
+
+namespace WorkerServiceApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CpuController : ControllerBase
+    {
+
+        [HttpPost]
+        public async Task<dynamic> CpuPorcentaje()
+        {
+            dynamic estadoCpu = "";
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT LoadPercentage FROM Win32_Processor");
+            ManagementObjectCollection results = searcher.Get();
+            foreach (ManagementObject obj in results)
+            {
+                int usage = Convert.ToInt32(obj["LoadPercentage"]);
+                estadoCpu = usage;
+            }
+            return estadoCpu;
+
+        }
+    }
+}
---
+++ WorkerServiceApi/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.Extensions.Hosting.WindowsServices;
+
+var options = new WebApplicationOptions
+{
+    Args = args,
+    ContentRootPath = WindowsServiceHelpers.IsWindowsService() ? AppContext.BaseDirectory : default
+};
+
+var builder = WebApplication.CreateBuilder(options);
+
+// Add services to the container.
+builder.Host.UseWindowsService();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
